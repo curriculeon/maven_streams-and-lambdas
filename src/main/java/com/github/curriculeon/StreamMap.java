cc4f834a1f555e713278759a4e80bcf7940af4d4
bwcsemaj <bwc_semaj@live.com>
2020-08-03 14:59:50 -0400

    Finished StreamMap

--- src/main/java/com/github/curriculeon/StreamMap.java
+++ src/main/java/com/github/curriculeon/StreamMap.java
@@ -1,5 +1,6 @@
 package com.github.curriculeon;
 
+import java.util.function.UnaryOperator;
 import java.util.stream.Stream;
 
 /**
@@ -10,24 +11,24 @@ public class StreamMap {
      * Section 8.3
      * @param someWord - word to convert to Stream<String>
      * @return - a Stream of single characters
-     */ //TODO
+     */
     public static Stream<String> letters(String someWord) {
-        return null;
+        return Stream.of(someWord.split(""));
     }
 
     /**
      * @param someWords - variable amount of String arguments
      * @return - a Stream of several Streams of single characters
-     */ //TODO
+     */
     public static Stream<Stream<String>> wordsMap(String... someWords) {
-        return null;
+        return Stream.of(someWords).map(StreamMap::letters);
     }
 
     /**
      * @param stringArray - variable amount of String arguments
      * @return - a Stream of several Streams of single characters
-     */ //TODO
+     */
     public static Stream<String> wordsFlatMap(String... stringArray) {
-        return null;
+        return wordsMap().flatMap(UnaryOperator.identity());
     }
 }
