9f32b9ae17b7cf506f700fcc582758747fb34efb
monicadeshmukh <renavikar_monica@yahoo.com>
2020-08-03 20:50:39 -0400

    ArrayConvertor completed

--- src/main/java/com/github/curriculeon/conversions/ArrayConverter.java
+++ src/main/java/com/github/curriculeon/conversions/ArrayConverter.java
@@ -3,11 +3,16 @@ package com.github.curriculeon.conversions;
 import com.github.curriculeon.anthropoid.Person;
 import com.github.curriculeon.anthropoid.PersonFactory;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
 /**
  * Created by leon on 5/25/17.
+ */
+
+/**
+ * TODO s implemented by Monica Deshmukh 8/3/2020
  */
 public final class ArrayConverter extends PersonConversionAgent<Person[]> {
     public ArrayConverter(Person... people) {
@@ -23,12 +28,13 @@ public final class ArrayConverter extends PersonConversionAgent<Person[]> {
 
     //TODO
     public List<Person> toList() {
-        return null;
+        return Arrays.asList(this.toArray());
+        //return null;
     }
 
     //TODO
     public Stream<Person> toStream() {
-        return null;
+        return Stream.of(this.toArray());
     }
 
     @Override
